89a1d936dc233e776962b1ae910ba349d0803842
Anas-Allouch <anasallouch10@gmail.com>
2025-11-18 22:37:05 +0100

    Update InputShaft.m

--- 01_First_Design/01_MATLAB_Calculations/moment_diagrams/InputShaft.m
+++ 01_First_Design/01_MATLAB_Calculations/moment_diagrams/InputShaft.m
@@ -1,5 +1,9 @@
 function res = InputShaft( range_Fy, range_Fz, features_range )
-File_F = 'C:\Users\anasa\OneDrive\Desktop\Gearbox Project\Excel Files\Important\input shaft forces.xlsx';
+scriptPath = mfilename('fullpath');
+[scriptDir, ~, ~] = fileparts(scriptPath);
+excelDir = fullfile(scriptDir, '..', '..', '02_Excel_Tables');
+
+File_F = fullfile(excelDir, 'input shaft forces.xlsx');
 Fy = xlsread(File_F, 1, range_Fy);
 Fz = xlsread(File_F, 1, range_Fz);
 locations = [0 38.23 97.08 134 193 229.93 288.78 327];
@@ -9,7 +13,8 @@ length = 0.327;
 [max_Mxz, x_Mz_all, Mz_all] = MomentDiagram(length, Fz, locations, 'Mxz');
 disp(max_Mxy);
 disp(max_Mxz);
-filename = 'C:\Users\anasa\OneDrive\Desktop\Gearbox Project\Excel Files\Important\input shaft features.xlsx';
+
+filename = fullfile(excelDir, 'input shaft features.xlsx');
 positions = xlsread(filename, 1, features_range);
 positions = positions.*(0.001);
 Mxy = Values(x_My_all, My_all, positions);
@@ -27,4 +32,5 @@ grid on;
 ylim([0 1000]);
 res = [max_Mxy max_Mxz];
 end
+
 
