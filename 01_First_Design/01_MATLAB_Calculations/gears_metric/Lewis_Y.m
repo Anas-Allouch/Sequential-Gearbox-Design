a6ac8cd88aed825c2aa341356b569071e30a5ae2
Anas-Allouch <anasallouch10@gmail.com>
2025-11-18 22:53:42 +0100

    Update Lewis_Y.m

--- 01_First_Design/01_MATLAB_Calculations/gears_metric/Lewis_Y.m
+++ 01_First_Design/01_MATLAB_Calculations/gears_metric/Lewis_Y.m
@@ -1,9 +1,13 @@
 function res = Lewis_Y( z )
-    filename = 'C:\Users\anasa\OneDrive\Desktop\Gearbox Project\Excel Files\Lewis Form Factor.xlsx';
+    scriptPath = mfilename('fullpath');
+    [scriptDir, ~, ~] = fileparts(scriptPath);
+    excelDir = fullfile(scriptDir, '..', '..', '02_Excel_Tables');
+    filename = fullfile(excelDir, 'Lewis Form Factor.xlsx');
     x_range = 'B3:B27';
     y_range = 'C3:C27';
     x = xlsread(filename, 1, x_range);
     y = xlsread(filename, 1, y_range);
     res = interp1(x, y, z, 'linear');
 end
+
 
