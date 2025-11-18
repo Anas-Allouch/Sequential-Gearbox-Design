2e2c5a428c7f8725e8e05ceda2352a1207855b6a
Anas-Allouch <anasallouch10@gmail.com>
2025-11-18 23:05:54 +0100

    Update main.m

--- 01_First_Design/01_MATLAB_Calculations/shaft_safety_factors/main.m
+++ 01_First_Design/01_MATLAB_Calculations/shaft_safety_factors/main.m
@@ -1,7 +1,11 @@
 %% input shaft reverse gear
 clear all
 clc
-file            = 'C:\Users\anasa\OneDrive\Desktop\Gearbox Project\Excel Files\Important\input shaft features.xlsx';
+scriptPath = mfilename('fullpath');
+[scriptDir, ~, ~] = fileparts(scriptPath);
+excelDir = fullfile(scriptDir, '..', '..', '02_Excel_Tables');
+
+file            = fullfile(excelDir, 'input shaft features.xlsx');
 Moment_range    = 'F4:F21';
 diam_range      = 'L4:L21';
 notch_range     = 'M4:M21';
@@ -27,7 +31,11 @@ end
 %% input shaft gear 1
 clear all
 clc
-file            = 'C:\Users\anasa\OneDrive\Desktop\Gearbox Project\Excel Files\Important\input shaft features.xlsx';
+scriptPath = mfilename('fullpath');
+[scriptDir, ~, ~] = fileparts(scriptPath);
+excelDir = fullfile(scriptDir, '..', '..', '02_Excel_Tables');
+
+file            = fullfile(excelDir, 'input shaft features.xlsx');
 Moment_range    = 'F26:F43';
 diam_range      = 'L26:L43';
 notch_range     = 'M26:M43';
@@ -53,7 +61,11 @@ end
 %% input shaft gear 2
 clear all
 clc
-file            = 'C:\Users\anasa\OneDrive\Desktop\Gearbox Project\Excel Files\Important\input shaft features.xlsx';
+scriptPath = mfilename('fullpath');
+[scriptDir, ~, ~] = fileparts(scriptPath);
+excelDir = fullfile(scriptDir, '..', '..', '02_Excel_Tables');
+
+file            = fullfile(excelDir, 'input shaft features.xlsx');
 Moment_range    = 'F48:F65';
 diam_range      = 'L48:L65';
 notch_range     = 'M48:M65';
@@ -79,7 +91,11 @@ end
 %% input shaft gear 3
 clear all
 clc
-file            = 'C:\Users\anasa\OneDrive\Desktop\Gearbox Project\Excel Files\Important\input shaft features.xlsx';
+scriptPath = mfilename('fullpath');
+[scriptDir, ~, ~] = fileparts(scriptPath);
+excelDir = fullfile(scriptDir, '..', '..', '02_Excel_Tables');
+
+file            = fullfile(excelDir, 'input shaft features.xlsx');
 Moment_range    = 'F70:F87';
 diam_range      = 'L70:L87';
 notch_range     = 'M70:M87';
@@ -105,7 +121,11 @@ end
 %% input shaft gear 4
 clear all
 clc
-file            = 'C:\Users\anasa\OneDrive\Desktop\Gearbox Project\Excel Files\Important\input shaft features.xlsx';
+scriptPath = mfilename('fullpath');
+[scriptDir, ~, ~] = fileparts(scriptPath);
+excelDir = fullfile(scriptDir, '..', '..', '02_Excel_Tables');
+
+file            = fullfile(excelDir, 'input shaft features.xlsx');
 Moment_range    = 'F92:F109';
 diam_range      = 'L92:L109';
 notch_range     = 'M92:M109';
@@ -131,7 +151,11 @@ end
 %% input shaft gear 5
 clear all
 clc
-file            = 'C:\Users\anasa\OneDrive\Desktop\Gearbox Project\Excel Files\Important\input shaft features.xlsx';
+scriptPath = mfilename('fullpath');
+[scriptDir, ~, ~] = fileparts(scriptPath);
+excelDir = fullfile(scriptDir, '..', '..', '02_Excel_Tables');
+
+file            = fullfile(excelDir, 'input shaft features.xlsx');
 Moment_range    = 'F114:F131';
 diam_range      = 'L114:L131';
 notch_range     = 'M114:M131';
@@ -157,7 +181,11 @@ end
 %% output shaft reverse gear
 clear all
 clc
-file            = 'C:\Users\anasa\OneDrive\Desktop\Gearbox Project\Excel Files\Important\output shaft features.xlsx';
+scriptPath = mfilename('fullpath');
+[scriptDir, ~, ~] = fileparts(scriptPath);
+excelDir = fullfile(scriptDir, '..', '..', '02_Excel_Tables');
+
+file            = fullfile(excelDir, 'output shaft features.xlsx');
 Moment_range    = 'F4:F14';
 diam_range      = 'L4:L14';
 notch_range     = 'M4:M14';
@@ -183,7 +211,11 @@ end
 %% output shaft gear 1
 clear all
 clc
-file            = 'C:\Users\anasa\OneDrive\Desktop\Gearbox Project\Excel Files\Important\output shaft features.xlsx';
+scriptPath = mfilename('fullpath');
+[scriptDir, ~, ~] = fileparts(scriptPath);
+excelDir = fullfile(scriptDir, '..', '..', '02_Excel_Tables');
+
+file            = fullfile(excelDir, 'output shaft features.xlsx');
 Moment_range    = 'F19:F29';
 diam_range      = 'L19:L29';
 notch_range     = 'M19:M29';
@@ -209,7 +241,11 @@ end
 %% output shaft gear 2
 clear all
 clc
-file            = 'C:\Users\anasa\OneDrive\Desktop\Gearbox Project\Excel Files\Important\output shaft features.xlsx';
+scriptPath = mfilename('fullpath');
+[scriptDir, ~, ~] = fileparts(scriptPath);
+excelDir = fullfile(scriptDir, '..', '..', '02_Excel_Tables');
+
+file            = fullfile(excelDir, 'output shaft features.xlsx');
 Moment_range    = 'F34:F44';
 diam_range      = 'L34:L44';
 notch_range     = 'M34:M44';
@@ -235,7 +271,11 @@ end
 %% output shaft gear 3
 clear all
 clc
-file            = 'C:\Users\anasa\OneDrive\Desktop\Gearbox Project\Excel Files\Important\output shaft features.xlsx';
+scriptPath = mfilename('fullpath');
+[scriptDir, ~, ~] = fileparts(scriptPath);
+excelDir = fullfile(scriptDir, '..', '..', '02_Excel_Tables');
+
+file            = fullfile(excelDir, 'output shaft features.xlsx');
 Moment_range    = 'F49:F59';
 diam_range      = 'L49:L59';
 notch_range     = 'M49:M59';
@@ -261,7 +301,11 @@ end
 %% output shaft gear 4
 clear all
 clc
-file            = 'C:\Users\anasa\OneDrive\Desktop\Gearbox Project\Excel Files\Important\output shaft features.xlsx';
+scriptPath = mfilename('fullpath');
+[scriptDir, ~, ~] = fileparts(scriptPath);
+excelDir = fullfile(scriptDir, '..', '..', '02_Excel_Tables');
+
+file            = fullfile(excelDir, 'output shaft features.xlsx');
 Moment_range    = 'F64:F74';
 diam_range      = 'L64:L74';
 notch_range     = 'M64:M74';
@@ -287,7 +331,11 @@ end
 %% output shaft gear 5
 clear all
 clc
-file            = 'C:\Users\anasa\OneDrive\Desktop\Gearbox Project\Excel Files\Important\output shaft features.xlsx';
+scriptPath = mfilename('fullpath');
+[scriptDir, ~, ~] = fileparts(scriptPath);
+excelDir = fullfile(scriptDir, '..', '..', '02_Excel_Tables');
+
+file            = fullfile(excelDir, 'output shaft features.xlsx');
 Moment_range    = 'F79:F89';
 diam_range      = 'L79:L89';
 notch_range     = 'M79:M89';
@@ -309,19 +357,3 @@ end
 for i=1:1:size(Mmax)
     disp(n_SF(i, :));
 end
-
-%% coupling keyseat
-clear all
-clc
-Sut     = 1250;
-Sy      = 1150;
-Mmax    = 0;
-Tmax    = 450;
-diam    = 37; % D = 40
-notch   = 1.5; % relief groove with a radius of 1.5
-kt      = 1.88;
-kts     = 1.3;
-flag    = 1;
-n_SF    = SafetyFactors(Sut, Sy, Mmax, Tmax, diam, notch, kt, kts, flag)
-Tmax    = 1457.1;
-n_SF    = SafetyFactors(Sut, Sy, Mmax, Tmax, diam, notch, kt, kts, flag)
